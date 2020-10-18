13a1f965eafde2855bc5494188af3a7b9c917f25
irwanadw <irwanardiansyahadw@gmail.com>
2020-10-18 15:32:03 +0700

    menambahkan getPromo.js

--- routes/admin/promos/getPromo.js
+++ routes/admin/promos/getPromo.js
@@ -3,8 +3,17 @@ const app = express.Router()
 const db = require('../../../controller/dbController')
 
 app.get('/admin/promos', (req, res) => {
-    id = req.query.id
-    res.send(db.get('promos', (id)))
+    let id = Number(req.query.id);
+    if (!isNaN(id)) {
+        const getbyId = db.get('promos', id);
+        if (getbyId) {
+            res.send(getbyId);
+        } else {
+            res.status(404).send("Data tidak ditemukan ");
+        }
+    } else {
+        res.status(404).send("Data tidak ditemukan ");
+    }
 })
 
 module.exports = app
--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const rootRoute = require('./routes/rootRoute')
 const getPromos = require('./routes/admin/promos/getPromo')
+const deletePromos = require('./routes/admin/promos/deletePromos')
+const addPromos = require('./routes/admin/promos/addPromos')
+const updatePromos = require('./routes/admin/promos/updatePromos')
 
 const app = express()
 app.use(rootRoute)
 app.use(getPromos)
+app.use(deletePromos)
+app.use(addPromos)
+app.use(updatePromos)
 
 app.use(bodyParser.json())
 
