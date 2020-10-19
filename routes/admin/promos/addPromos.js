69bd1a069a86f514b4bb7262a91ba38b5f5a3794
irwanadw <irwanardiansyahadw@gmail.com>
2020-10-19 08:50:00 +0700

    menambahkan add dan edit yang tidak work

--- routes/admin/promos/addPromos.js
+++ routes/admin/promos/addPromos.js
@@ -2,19 +2,51 @@ const express = require('express')
 const db = require('../../../controller/dbController')
 const app = express.Router()
 
-app.post('/admin/promos', (req, res) => {
-    id = Number(req.query.id);
 
-    if (!isNaN(id)) {
-        isIdAvailable = db.get('promos', id);
-        if (isIdAvailable) {
-            res.status(409).send("You cannot adding something on the same ID!");
-        } else {
-            db.add('promos', body);
-            res.send(body);
+app.post('/admin/promos', (req, res) => {
+    const body = req.body
+    const { id, promoName, promoStartDate, endStartDate } = body
+    const promos = db.get('promos')
+
+    const data = ['id', 'promoName', 'promoStartDate', 'endStartDate']
+    const i = []
+    for (const keys in body) {
+        if ((data.includes(keys)) && !(i.includes(keys))) {
+            i.push(keys)
         }
-    } else {
-        res.status(400).send("You must enter an ID which is a number!");
+
+        const isIdNan = isNaN(id)
+        if (isIdNan) {
+            res.status(409).send('You insert a wrong format id')
+            return
+        }
+
+        const isStartDateValid = dayjs(promoStartDate).format('YYYY-MM-DD') != promoStartDate
+        if (isStartDateValid) {
+            res.status(409).send('Date Start is not Valid')
+            return
+        }
+
+        const isEndDateValid = dayjs(endStartDate).format('YYYY-MM-DD') != endStartDate
+        if (isEndDateValid) {
+            res.status(409).send('Date End is not Valid')
+            return
+        }
+
+        const isIdAvailable = db.get('promos', id)
+        if (isIdAvailable) {
+            res.status(409).send('You insert an existed Id')
+        } else {
+            const input = {
+                'id': id,
+                'promoName': promoName,
+                'promoStartDate': promoStartDate,
+                'endStartDate': endStartDate
+            }
+            db.add('promos', input)
+            res.send(body)
+        }
+
     }
 })
 
--- routes/admin/promos/editPromos.js
+++ routes/admin/promos/editPromos.js
@@ -3,21 +3,47 @@ const db = require('../../../controller/dbController')
 const app = express.Router()
 
 app.patch('/admin/promos', (req, res) => {
-    body = req.body
-    id = Number(body.id)
-    parsedId = parseInt(id)
-    if (!isNaN(id)) {
-        isIdAvailable = db.get('promos', id)
+    const body = req.body
+    const { id, promoStartDate, endStartDate } = body
 
-        if (isIdAvailable) {
-            db.edit('promos', id, body)
-            res.send(body)
-        } else {
-            res.status(404).send("Data not Found")
-        }
-    } else {
-        res.status(400).send("You must enter an ID which is a number!");
+    const isBodyObject = isObject(body)
+    if (!body && !isBodyObject) {
+        res.status(400).send('Wrong payload')
+        return
     }
+
+    // cek body valid
+    const isBodyValid = checkObject(body, data)
+    if (!isBodyValid) {
+        res.status(400).send('Data structure not valid')
+        return
+    }
+
+    // cek date valid
+    const isStartDateValid = dayjs(promoStartDate).format('YYYY-MM-DD') != promoStartDate
+    if (isStartDateValid) {
+        res.status(409).send('Date Start is not Valid')
+        return
+    }
+
+    const isEndDateValid = dayjs(endStartDate).format('YYYY-MM-DD') != endStartDate
+    if (isEndDateValid) {
+        res.status(409).send('Date End is not Valid')
+        return
+    }
+
+    // cek id available
+    const isIdAvailable = db.get('promos', id)
+    if (!isIdAvailable) {
+        res.status(404).send('Id not exists')
+        return
+    }
+
+    db.edit('promos', id, body)
+    body.id = id
+    res.send(body)
+    return
 })
+
 
 module.exports = app
